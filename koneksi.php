ce2d3f77c725d6475c68381ced54ca7dc0a97f28
Ahmad Makhi <59573668+makhysco@users.noreply.github.com>
2020-10-30 22:37:22 +0700

    Create koneksi.php

---
+++ koneksi.php
@@ -0,0 +1,9 @@
+<?php
+    $host = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "akademik";
+
+    $koneksi = mysqli_connect($host, $username, $password, $database) or die ("koneksi ke database gagal!")
+
+?>
